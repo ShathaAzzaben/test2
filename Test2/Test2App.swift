c8c0b8ac8ec020c8c19f04d4a7bc92529cb7b2d6
ShathaAzzaben <ShathaAzzaben@gmail.com>
2025-04-17 13:27:40 +0300

    Initial Commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Shatha Azzaben on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test2Tests/Test2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Test2Tests.swift
+//  Test2Tests
+//
+//  Created by Shatha Azzaben on 17/04/2025.
+//
+
+import Testing
+@testable import Test2
+
+struct Test2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
